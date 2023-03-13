4c3fc22db982edb4ef8025b3eb317c8e0fa5cb89
Grandman7777 <kokaiya7777@gmail.com>
2023-03-13 22:10:38 +0200

    Childrens

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const root = document.querySelector('#root');
-const h1 = React.createElement('h1', {}, "hello Bro");
+class HeaaderComponent extends React.Component {
+    render() {
+        console.log(this.props)
+        const p = React.createElement('p', {}, 'Super paragraph');
+        const h2 = React.createElement('h2', {title: 'Hi'}, `Hello ${this.props}`);
+        const article = React.createElement('article', {}, h2, p);
+        return article;
+    }
+}
 
-ReactDOM.render(h1, root);  // заапендили h1 к root 
- 
+const component = React.createElement(HeaaderComponent, {name: "React Boy"}, 'Text');
+const component2 = React.createElement(HeaaderComponent, {name: 'Java Boy'});
+const parentElement = React.createElement('section', {}, component, component2);
+
+const root = document.querySelector('#root');
+ReactDOM.render(parentElement, root);
